b1b7b2a526f1f6b308ef3afbc80a855dc28c339e
Shrey K <srkkadia@gmail.com>
2024-06-03 20:07:51 +0530

    Introduction to Python, Basic Print Statement

---
+++ Basics/FirstProgram.py
@@ -0,0 +1,20 @@
+# single line comment
+'''
+multi
+line
+comment
+'''
+
+"""
+multi
+line
+comment
+"""
+'''
+print statement:
+syntax:
+    (*objects, sep=' ', end="\n", file=file.sysout, flush=False) -> None
+
+'''
+
+print("To Be Continued...")
