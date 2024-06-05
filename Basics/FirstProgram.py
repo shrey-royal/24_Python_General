8976fc9c4327df1ec2f26a7146b76a0b1783eff8
Shrey K <srkkadia@gmail.com>
2024-06-05 20:58:51 +0530

    Print Statement, Data Types, Variables

--- Basics/FirstProgram.py
+++ Basics/FirstProgram.py
@@ -17,4 +17,45 @@ syntax:
 
 '''
 
-print("To Be Continued...")
+# print("this is an object", '\t', "this is another line", end='\t')
+# print("Apple", "Banana", "Cherry", sep="_")
+
+# Data Types
+"""
+
+1. Text: str
+2. Numeric: int, float, complex(117j)
+3. Sequence: list, tuple, range
+4. Mapping: dict
+5. Set: set, frozenset
+6. Boolean: bool
+7. Binary: bytes(binary), bytearray(array of binary nums)
+8. None: NoneType
+
+"""
+
+print("\"Hello World!\"")
+# print('"""Hello World!"""')
+# print("\"\"\"Hello World!\"\"\"")
+
+# id() - used to verify the identity of any variable
+# type() - used to identify the data type (return classes)
+
+# Variables
+
+# a = "str"
+# b = "str1"
+# print(id(a))
+# print(id(b))
+# print(type(a))
+
+# a = 12      # int
+# a = 12.6    # float
+# a = 1-12j   # complex
+# a = "str"   # str
+# a = True    # bool (True/False)
+# a = b'\x01' # bytes
+# a = None
+a = 12
+
+print(id(a), '-> ', type(a), '-> ', a)
