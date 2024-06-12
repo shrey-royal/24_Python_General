7626a9bd3e758c0b29cb68f566c8b0763e78341a
Shrey K <srkkadia@gmail.com>
2024-06-12 21:12:27 +0530

    User Input and String Methods

---
+++ Basics/ScanData.py
@@ -0,0 +1,12 @@
+'''
+> input fn is used to scan data from the user (default input type is str)
+syntax:
+    var = input("prompt to user") # type str
+'''
+
+# var = input("Enter str: ")
+
+# var = int(input("Enter int: "))
+var = float(input("Enter float: "))
+
+print(type(var), var)
---
+++ Basics/String_methods.py
@@ -0,0 +1,47 @@
+# text = "hello world"
+# print(text.capitalize())
+
+# text = "pYtHoN pRoGRamMInG"
+# print(text.capitalize())
+
+# text = "HELLO WORLD"
+# print(text.casefold())
+
+# text = "Zafar Sir"
+# print(f"'{text.center(50, '_')}'")
+
+# text = "hello world"
+# print(text.count('l', 2, 5))
+
+# text = "cafē"
+# print(text.encode(encoding='ascii', errors='strict'))
+
+# text = "python programming is fun"
+# print(text.endswith('fun'))
+# print("Hello\tWorld abc".expandtabs(8))
+
+# print("hello world".find('o', 5))
+# print("python programming".find('prog'))
+
+# text = "Good {}!"
+# print(text.format("Evening"))
+# print("Addition of {} and {} is {}".format(5, 3, (5+3)))
+# print("Addition of {0} and {1} is {2}".format(5, 3, 5+3))
+
+# print("hello world".index('o'))
+# print("HELLO124".isupper())
+# print("hello124".islower())
+# print("hello".isalpha())
+# print("1he2llo12".isalnum())
+# print("1he2llo12".isascii())
+# print("9992399".isdigit())
+# print("9992399".isdecimal())
+# print("9992399".isnumeric())
+# print("_1var".isidentifier())
+# print("cafē\n".isprintable())
+# print("  ".isspace())
+# print(" Hello World".istitle())
+# print("Zafar Sir".join("123"))
+# print(" ".join(" Zafar Sir "))
+# print("'", "hello".ljust(10, '-'), "'", sep="")
+# print("-*-*-*-*-*-*-*-*-*-*-*-*-hello".lstrip('*-|'))
