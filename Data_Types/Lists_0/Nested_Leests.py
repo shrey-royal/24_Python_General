4148ab1b3dd610cf969766d61fe4e3006ba0d871
Shrey K <srkkadia@gmail.com>
2024-07-03 20:58:51 +0530

    Nested Lists

--- Data_Types/Lists_0/Nested_Leests.py
+++ Data_Types/Lists_0/Nested_Leests.py
@@ -7,4 +7,28 @@ var = [data.veggies, data.fruits]
 # print(type(var[0][0]))
 # print(var[0][0])
 
-print(var[0][18])
+# print(var[0][18])
+
+# var[1].clear()
+# print(var)
+
+# for i in var:
+#     for j in i:
+#         print(j)
+
+# list_index = 0
+# while list_index < len(var):     # var == var
+#     index = 0
+#     while index < len(var[list_index]):
+#         print(f"{list_index}, {index} -> {var[list_index][index]}")
+#         index += 1
+#     list_index += 1
+
+# flatted_lists = [item for list_item in var for item in list_item]
+
+# print(flatted_lists)
+
+# var[1][0] = ""
+# var[0].append("abcd")
+
+# print(var)
