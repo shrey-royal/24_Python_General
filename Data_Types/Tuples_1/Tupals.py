0af4aa6b4eef0ce49383f4416a9d250d30f4df81
Shrey K <srkkadia@gmail.com>
2024-07-08 19:59:17 +0530

    Tuples

---
+++ Data_Types/Tuples_1/Tupals.py
@@ -0,0 +1,32 @@
+"""
+Tuple -> Immutable Sequence
+
+syntax:
+    tuple() -> represented by ()
+
+Changeable? -> False
+Duplicates Allowed? -> True
+
+"""
+
+# myTuple = (3, 5, 2, 56, 8, 4, 3, 12, 5, 78, "2", ["2", 34, 2, 2])
+# print(myTuple, type(myTuple), id(myTuple))
+# print(id(myTuple))
+
+# print("Index:", myTuple.index(2, 6, 12))
+# print("Len:", len(myTuple))
+# print("Count:", myTuple.count(2))
+
+# myTuple[11][0] = 23
+
+# print(myTuple)
+
+
+# Ordering tuple by list
+"""
+Input:
+tupList = [('l', 5), ('k', 2), ('a', 1), ('e', 6)], sortList = ['l', 'a', 'k', 'e']
+
+Output:
+[('l', 5), ('a', 1), ('k', 2), ('e', 6)]
+"""
