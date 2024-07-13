195807e828da71c041b4d5ee1639cabd7ce89cd6
Shrey K <srkkadia@gmail.com>
2024-07-13 16:09:38 +0530

    Tuples_Example

--- Data_Types/Tuples_1/Tupals.py
+++ Data_Types/Tuples_1/Tupals.py
@@ -30,3 +30,37 @@ tupList = [('l', 5), ('k', 2), ('a', 1), ('e', 6)], sortList = ['l', 'a', 'k', '
 Output:
 [('l', 5), ('a', 1), ('k', 2), ('e', 6)]
 """
+# tupList = [('l', 5), ('k', 2), ('a', 1), ('e', 6)]
+# sortList = ['l', 'a', 'k', 'e']
+# output = [t for k in sortList for t in tupList if t[0] == k]
+# print(output)
+
+
+'''
+Example 2:
+Python program to convert decimal to binary (in bytes)
+Input:
+tuple1 = (1234, 331, 437, 59, 63)
+
+Output:
+(binary of 1, binary of 2, binary of 3, binary of 4, binary of 5)
+'''
+
+# num = 25
+# ans = ""
+
+# while num > 0:
+#     ans += str(num%2)
+#     num//=2
+
+# print(ans[::-1].zfill(8))
+
+# print(bin(25)[2:])
+
+
+tuple1 = (1234, 331, 437, 59, 63)
+tuple2 = (1, 3, 5, 2, 5)
+
+for i in range(len(tuple1)):
+    # print(bin(tuple1[i])[2:])
+    print(tuple1[i] & tuple2[i], end=", ")
